e01e1313f58123687e502def7f969dcc3854ed22
st97110 <47217662+st97110@users.noreply.github.com>
2023-06-20 12:52:20 +0800

    UI and camera Adaptive
    camera change FOV
    UI use anchor point

--- mahjong/Assets/aspectCamera.cs
+++ mahjong/Assets/aspectCamera.cs
@@ -1,17 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class aspectCamera : MonoBehaviour
 {
-    public float baseWidth = 1024;
-    public float baseHeight = 768;
-    public float baseOrthographicSize = 390;
+    public float deviceWidth;
+    public float deviceHeight;
+    public float aspectRatio;
+    public Camera mainCamera;
 
-    void Update()
+    private void Awake()
     {
+        aspectRatio = deviceWidth / deviceHeight;
+    }
 
-        float newOrthographicSize = (float)Screen.height / (float)Screen.width * this.baseWidth / this.baseHeight * this.baseOrthographicSize;
-        GetComponent<Camera>().orthographicSize = Mathf.Max(newOrthographicSize, this.baseOrthographicSize);
+    private void Update()
+    {
+        deviceWidth = Screen.width;
+        deviceHeight = Screen.height;
+        aspectRatio = deviceWidth / deviceHeight;
+        float targetAspectRatio = 1792f / 828f;
+        float baseFOV = 55f;
+        float extraWidth = 0f;
+        float extraHeight = 0f;
+
+        if (aspectRatio > targetAspectRatio)
+        {
+            // ??的?高比例??，需要添加黑?在左右
+            extraWidth = deviceHeight * targetAspectRatio - deviceWidth;
+        }
+        else if (aspectRatio < targetAspectRatio)
+        {
+            // ??的?高比例?窄，需要添加黑?在上下
+            extraHeight = deviceWidth / targetAspectRatio - deviceHeight;
+        }
+
+        Debug.Log(deviceWidth);
+        Debug.Log(deviceHeight);
+        float targetFOV = baseFOV * (targetAspectRatio / aspectRatio);
+        mainCamera.fieldOfView = targetFOV;
+
+        // 根据需要添加的黑?大小?整相机的?野范?
+        mainCamera.rect = new Rect(
+            extraWidth / (2f * deviceWidth),
+            extraHeight / (2f * deviceHeight),
+            1f - extraWidth / deviceWidth,
+            1f - extraHeight / deviceHeight
+        );
+
+        // 在相机的背景上添加黑?效果
+        mainCamera.backgroundColor = Color.black;
     }
 }
