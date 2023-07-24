56a0579c9cc1a80bb86bf3738de2e8a65d7d6fb5
Manual-debuger <103013135+Manual-debuger@users.noreply.github.com>
2023-07-24 18:45:35 +0800

    Update GameManger.cs

--- mahjong/Assets/Scripts/GameManger.cs
+++ mahjong/Assets/Scripts/GameManger.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameManger : MonoBehaviour
 {
+    private static GameManger _instance = new GameManger();
+    private GameManger() { }
+    public static GameManger Instance { get { return _instance; } }
+    private AnimController _animController;
+    private AudioController _audioManager;
+    
     // Start is called before the first frame update
     void Start()
     {
