c37a206840be265f72fe748c3ee23e3e3d47a5e8
Manual-debuger <103013135+Manual-debuger@users.noreply.github.com>
2023-08-08 17:03:35 +0800

    中央區跟玩家諮詢plate的script配置完成

--- mahjong/Assets/Scripts/CentralAreaController.cs
+++ mahjong/Assets/Scripts/CentralAreaController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //Duty: 處理中間的贏分，風位，莊家等等
-public class CentralAreaController : MonoBehaviour
+public class CentralAreaController : MonoBehaviour,IInitiable
 {
+    [SerializeField] private TextMeshPro _numberOfRemainedTilesTextMeshPro;    
+    [SerializeField] private List<TextMeshPro> _scoresTextMeshProList; // Default ESWN
+    [SerializeField] private List<TextMeshPro> _dealersTextMeshProList;// Default ESWN
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +20,34 @@ public class CentralAreaController : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Init()
+    {
+        foreach(var scoreTextMeshPro in _scoresTextMeshProList)
+        {
+            scoreTextMeshPro.text = "0";
+        }
+        foreach(var dealerTextMeshPro in _dealersTextMeshProList)
+        {
+            dealerTextMeshPro.text = "";
+        }
+        _numberOfRemainedTilesTextMeshPro.text = "88";
+        throw new System.NotImplementedException();
+    }
+    public void SetScore()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void SetDealer()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void SetNumberOfRemainedTiles(int number)
+    {
+        _numberOfRemainedTilesTextMeshPro.text = number.ToString();
+    }
+    public void ReduceNumberOfRemainedTilesByOne()
+    {
+        throw new System.NotImplementedException();
     }
 }
--- mahjong/Assets/Scripts/PlayerInfoPlateController.cs
+++ mahjong/Assets/Scripts/PlayerInfoPlateController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInfoPlateController : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer _userAvatarMeshRenderer;
+    [SerializeField] private TextMeshPro _userNameTextMeshPro;
+    [SerializeField] private TextMeshPro _windPosisionTextMeshPro;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,17 @@ public class PlayerInfoPlateController : MonoBehaviour
     void Update()
     {
         
+    }
+    public void SetUserName(string name)
+    {
+        _userNameTextMeshPro.text = name;
+    }
+    public void SetWindPosision(string wind)
+    {
+        _windPosisionTextMeshPro.text = wind;
+    }
+    public void SetUserAvaterPhoto(Material material)
+    {
+        _userAvatarMeshRenderer.materials[0] = material;
     }
 }
