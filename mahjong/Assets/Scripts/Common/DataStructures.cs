eddbdeebef283a3adec9829556e1522487858d0e
Manual-debuger <103013135+Manual-debuger@users.noreply.github.com>
2023-08-11 11:18:34 +0800

    Add EventArgs, Modify GameManger
    EventArgs in  Common/DataStructures

---
+++ mahjong/Assets/Scripts/Common/DataStructures.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+#region Event Args
+public class DiscardTileEventArgs:EventArgs
+{
+    public TileSuits TileSuits;
+    public int PlayerIndex;
+    public DiscardTileEventArgs(TileSuits tileSuits,int playerIndex)
+    {
+        TileSuits = tileSuits;
+        PlayerIndex = playerIndex;
+    }
+}
+
+public class ChowTileEventArgs : EventArgs
+{
+    public TileSuits TileSuits;
+    public int PlayerIndex;
+    public ChowTileEventArgs(TileSuits tileSuits, int playerIndex)
+    {
+        TileSuits = tileSuits;
+        PlayerIndex = playerIndex;
+    }
+}
+public class PongTileEventArgs : EventArgs
+{
+    public TileSuits TileSuits;
+    public int PlayerIndex;
+    public PongTileEventArgs(TileSuits tileSuits, int playerIndex)
+    {
+        TileSuits = tileSuits;
+        PlayerIndex = playerIndex;
+    }
+}
+public class KongTileEventArgs : EventArgs
+{
+    public bool IsConcealedKong;
+    public TileSuits TileSuits;
+    public int PlayerIndex;
+    public KongTileEventArgs(bool isConcealedKong,TileSuits tileSuits, int playerIndex)
+    {
+        TileSuits = tileSuits;
+        PlayerIndex = playerIndex;
+        IsConcealedKong = isConcealedKong;
+    }
+}
+public class DiscardTileSuggestArgs:DiscardTileEventArgs
+{
+    public DiscardTileSuggestArgs(TileSuits tileSuits, int playerIndex) : base(tileSuits, playerIndex)
+    {
+    }
+}
+public class WinningSuggestArgs : EventArgs
+{
+    public TileSuits TileSuits;
+    public int PlayerIndex;
+    public WinningSuggestArgs(TileSuits tileSuits, int playerIndex)
+    {
+        TileSuits = tileSuits;
+        PlayerIndex = playerIndex;
+    }
+}
+#endregion
--- mahjong/Assets/Scripts/GameManager.cs
+++ mahjong/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour,IInitiable
 {
@@ -11,27 +13,42 @@ public class GameManager : MonoBehaviour,IInitiable
     [SerializeField] private PlayerControllerBase[] _playerControllers;
     [SerializeField] private InGameUIController _inGameUIController;
     [SerializeField] private AnimController _animController;
-    [SerializeField] private AudioController _audioManager;
-    
+    [SerializeField] private AudioController _audioManager;    
+    public void Init()
+    {
+        throw new System.NotImplementedException();
+    }
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {       
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    //Example 參數還不確定
-    public void AddHandTile(int PlayerIndex,TileSuits tileSuits)
+    }  
+    #region UI Event handle
+    public void OnDiscardTileEvent(object sender,DiscardTileEventArgs e)
     {
         throw new System.NotImplementedException();
     }
+    public void OnChowTileEvent(object sender, ChowTileEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+    public void OnPongTileEvent(object sender, PongTileEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+    public void OnKongTileEvent(object sender, KongTileEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+    public void OnWinningSuggestEvent(object sender, WinningSuggestArgs e)
+    {
+        throw new System.NotImplementedException();
+    }    
+    #endregion
 
-    public void Init()
-    {
-        throw new System.NotImplementedException();
-    }
 }
